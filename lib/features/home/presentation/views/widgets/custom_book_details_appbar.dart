dab91408ba2d2117fdce4fc368a5724d54e2983e
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-03-24 03:57:32 +0200

    add app bar to book details view

---
+++ lib/features/home/presentation/views/widgets/custom_book_details_appbar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomBookDetailsAppBar extends StatelessWidget {
+  const CustomBookDetailsAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30, bottom: 30, top: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.close),
+          ),
+          const Icon(Icons.shopping_cart)
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_items_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_items_list_view.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -12,7 +12,7 @@ class HomeViewBody extends StatelessWidget {
     return const CustomScrollView(
       physics: BouncingScrollPhysics(),
       slivers: [
-        SliverToBoxAdapter(child: CustomAppBar()),
+        SliverToBoxAdapter(child: CustomHomeAppBar()),
         SliverToBoxAdapter(child: BookItemsListView()),
         SliverToBoxAdapter(
           child: Padding(
