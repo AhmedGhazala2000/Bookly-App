9b0e1c5b2d96bfc0f76634a1ca58a6632fa0400a
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-03-24 21:07:40 +0200

    Create search view and navigate on it

--- lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 
 class CustomHomeAppBar extends StatelessWidget {
   const CustomHomeAppBar({super.key});
@@ -8,7 +11,7 @@ class CustomHomeAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 30, right: 30, bottom: 50, top: 24),
+      padding: const EdgeInsets.only(left: 30, right: 30, bottom: 50, top: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -16,7 +19,16 @@ class CustomHomeAppBar extends StatelessWidget {
             AssetsImg.logo,
             height: 17,
           ),
-          const Icon(FontAwesomeIcons.magnifyingGlass),
+          IconButton(
+            onPressed: () {
+              Get.to(
+                () => const SearchView(),
+                transition: Transition.fadeIn,
+                duration: kTransitionDuration,
+              );
+            },
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass),
+          ),
         ],
       ),
     );
