5c3eef3669c5d6f09ee926c71878624c41a3eec4
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-03-26 17:12:46 +0200

    Implement home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,8 +1,8 @@
 import 'package:bookly_app/core/errors/failures.dart';
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/models/book_model/item.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
-  Future<Either<Failures, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failures, List<Item>>> fetchFeaturedBooks();
+  Future<Either<Failures, List<Item>>> fetchNewestBooks();
 }
---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,37 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/models/book_model/item.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
+  @override
+  Future<Either<Failures, List<Item>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      var books = BookModel.fromJson(data).items!;
+      return Right(books);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failures, List<Item>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming');
+      var books = BookModel.fromJson(data).items!;
+      return Right(books);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+}
