f07ab266210661777dac8449636982f5eefcbfd9
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-03-26 23:42:31 +0200

    Create service locator and use it in BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:bookly_app/constant.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -12,14 +19,22 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: kPrimaryColor,
-        fontFamily: 'Montserrat',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>())),
+        BlocProvider(
+            create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>())),
+      ],
+      child: GetMaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: kPrimaryColor,
+          fontFamily: 'Montserrat',
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SplashView(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const SplashView(),
     );
   }
 }
